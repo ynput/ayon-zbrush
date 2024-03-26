167b5814d5f7dccda330aefba568f11f3121c29e
moonyuet <moonyuet2008@hotmail.com>
2024-03-26 09:50:08 +0000

    fix the broken workfile creator

--- client/ayon_zbrush/plugins/create/create_workfile.py
+++ client/ayon_zbrush/plugins/create/create_workfile.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """Creator plugin for creating workfiles."""
+import ayon_api
 from ayon_core.pipeline import CreatedInstance
-from ayon_core.client import get_asset_by_name, get_asset_name_identifier
+from ayon_core.client import get_asset_by_name, get_folder_path_identifier
 from ayon_zbrush.api import plugin
 
 class CreateWorkfile(plugin.ZbrushAutoCreator):
@@ -21,7 +22,7 @@ class CreateWorkfile(plugin.ZbrushAutoCreator):
                 if instance.creator_identifier == self.identifier
             ), None)
         project_name = self.project_name
-        asset_name = self.create_context.get_current_asset_name()
+        folder_path = self.create_context.get_current_folder_path()
         task_name = self.create_context.get_current_task_name()
         host_name = self.create_context.host_name
 
@@ -31,22 +32,27 @@ class CreateWorkfile(plugin.ZbrushAutoCreator):
             current_instance_asset = current_instance["folderPath"]
 
         if current_instance is None:
-            asset_doc = get_asset_by_name(project_name, asset_name)
-            subset_name = self.get_product_name(
+            folder_entity = ayon_api.get_folder_by_path(
+                project_name, folder_path
+            )
+            task_entity = ayon_api.get_task_by_name(
+                project_name, folder_entity["id"], task_name
+            )
+            product_name = self.get_product_name(
                 project_name,
-                asset_doc,
-                task_name,
+                folder_entity,
+                task_entity,
                 variant,
-                host_name
+                host_name,
             )
             data = {
-                "task": task_name,
+                "task": task_entity,
                 "variant": variant,
-                "folderPath": asset_name
+                "folderPath": folder_path
             }
 
             new_instance = CreatedInstance(
-                self.product_type, subset_name, data, self
+                self.product_type, product_name, data, self
             )
             instances_data = self.host.list_instances()
             instances_data.append(new_instance.data_to_store())
@@ -54,16 +60,21 @@ class CreateWorkfile(plugin.ZbrushAutoCreator):
             self._add_instance_to_context(new_instance)
 
         elif (
-            current_instance_asset != asset_name
+            current_instance_asset != folder_path
             or current_instance["task"] != task_name
         ):
             # Update instance context if is not the same
-            asset_doc = get_asset_by_name(project_name, asset_name)
-            subset_name = self.get_subset_name(
-                variant, task_name, asset_doc, project_name, host_name
+            folder_entity = ayon_api.get_folder_by_path(
+                project_name, folder_path
             )
-            asset_name = get_asset_name_identifier(asset_doc)
+            task_entity = ayon_api.get_task_by_name(
+                project_name, folder_entity["id"], task_name
+            )
+            product_name = self.get_product_name(
+                variant, task_entity, folder_entity, project_name, host_name
+            )
+            folder_path = get_folder_path_identifier(folder_entity)
 
-            current_instance["folderPath"] = asset_name
-            current_instance["task"] = task_name
-            current_instance["subset"] = subset_name
+            current_instance["folderPath"] = folder_path
+            current_instance["task"] = task_entity
+            current_instance["subset"] = product_name
